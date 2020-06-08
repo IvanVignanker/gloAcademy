07881a393901ba09d8f32ccb15ae8c0b9f84aad3
Ivan Vignanker <i.vignanker@gmail.com>
2020-06-08 22:25:40 +0300

    ДЗ ко 2 уроку

---
+++ lesson02/script.js
@@ -0,0 +1,17 @@
+let money = 15000;
+let income = 5000;
+let addExpenses = "rent a flat, rent a car, food";
+let deposit = true;
+let mission = 500000;
+let period = 3;
+
+let budgetDay = money / 30 ;
+
+console.log(typeof(money));
+console.log(typeof(income ));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log("Период равен " + period +" месяцев");
+console.log("Цель заработать " + mission + " шекелей.");
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);

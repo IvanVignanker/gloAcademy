67e3eed3fd8babb704e4410f29d3d11f69d68ea4
Ivan Vignanker <i.vignanker@gmail.com>
2020-06-12 00:32:49 +0300

    Добавлена функция getStatusIncome

--- lesson04/script.js
+++ lesson04/script.js
@@ -27,7 +27,25 @@ function getTargetMonth (c, d){
 }
 let targetMonth = getTargetMonth(mission, accumulatedMonth);
 let budgetDay = Math.floor(accumulatedMonth / 30);
-function getStatusIncome(){}
+function getStatusIncome(){
+    switch (true){
+        case budgetDay > 1200 :
+            console.log('У вас высокий уровень дохода');
+            break;
+        case 1200 > budgetDay > 600 :
+            console.log('У вас средний уровень дохода');
+            break;
+        case budgetDay < 600 :
+            console.log('К сожалению у вас уровень дохода ниже среднего');
+            break;
+        case budgetDay < 0 :
+            console.log('Что то пошло не так');
+            break;
+        case budgetDay === 0 :
+            console.log('Вы не умрете от голода');
+            break;
+    };
+}
 
 console.log(typeof(money));
 console.log(typeof(income ));
